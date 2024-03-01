beab33d638f4297eba90bc4f4669f33e8c645fd7
Maksim9056 <bobreczovm@inbox.ru>
2024-03-01 14:20:27 +0300

    Добавьте файлы проекта.

---
+++ WcfService3/IService1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using static WcfService3.Service1;
+
+namespace WcfService3
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+
+        List<Contrack> ListContrat();
+        [OperationContract]
+
+        string AddGet(Contrack contrack);
+        [OperationContract]
+
+        string Push(int id);
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Добавьте здесь операции служб
+    }
+
+
+    // Используйте контракт данных, как показано в примере ниже, чтобы добавить составные типы к операциям служб.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WcfService3/Service1.svc.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Services.Description;
+
+namespace WcfService3
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде, SVC-файле и файле конфигурации.
+    // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы Service1.svc или Service1.svc.cs в обозревателе решений и начните отладку.
+    public class Service1 : IService1
+    {
+        public List<Contrack> Services = new List<Contrack>();
+        public string AddGet(Contrack contrack)
+        {
+
+            Services.Add(contrack);
+            return "Добавился";
+        }
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public string Push(int contrack)
+        {
+            Contrack contrack1 = Services.Find(find => find.Id == contrack);
+
+            return contrack1.Name_Contrack;
+        }
+
+        public class Contrack
+        {
+            public int Id { get; set; }
+            public string Name_Contrack { get; set; }
+            public Contrack(int id, string name_Contrack)
+            {
+                Id = id;
+                Name_Contrack = name_Contrack;
+            }
+
+        }
+
+
+        public List<Contrack> ListContrat()
+        {
+
+            return Services;
+        }
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+
+    }
+}
